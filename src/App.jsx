894f8ba4904b00add9d3a662c65c8d7fe26960d1
Lany Plaza <lanyplaza@gmail.com>
2023-07-09 20:11:22 -0300

    Clase 2

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Clases React</h1>
+      <h1>Clase 2 de React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
